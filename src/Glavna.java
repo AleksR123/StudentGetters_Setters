c8decf39af2990b27535a35b73badcc3ae267a34
unknown <aleksandarristov28@gmail.com>
2023-12-23 19:46:31 +0100

    first java program

---
+++ src/Glavna.java
@@ -0,0 +1,12 @@
+
+public class Glavna {
+public static void main(String[]args) {
+		
+		Studenti marko = new Studenti();
+		marko.setId(1);
+		marko.setIme("Marko");
+		marko.setGodini(18);
+		System.out.println("Reden broj " + marko.getId() + " e studentot " + marko.getIme() + " star " + marko.getGodini() + " godini");
+	
+	}
+ }
---
+++ src/Studenti.java
@@ -0,0 +1,27 @@
+
+public class Studenti {
+	int id;
+	String ime;
+	int godini;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public int getGodini() {
+		return godini;
+	}
+	public void setGodini(int godini) {
+		
+		this.godini = godini;
+	}
+	
+}
